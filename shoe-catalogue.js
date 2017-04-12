a7536875fbde3b3bc341f0014f56947352692f95
lihlepaie <pasielihle@gmail.com>
2017-04-12 13:01:45 +0200

    making onother changes on each file

--- shoe-catalogue.js
+++ shoe-catalogue.js
@@ -1,66 +1,18 @@
-var shoes = [{
-        color: 'Red',
-        price: 1200,
-        in_stock: 12,
-        size: 4,
-        brand: 'Nike'
-
-    },
-    {
-        color: 'Brown',
-        price: 750,
-        in_stock: 22,
-        size: 5,
-        brand: 'Adidas'
-
-    },
-
-
-    {
-        color: 'Green',
-        price: 550,
-        in_stock: 40,
-        size: 6,
-        brand: 'Supegar'
-
-
-
-    },
-    {
-        color: 'Blue',
-        price: 800,
-        in_stock: 17,
-        size: 7,
-        brand: 'Jordan'
-
-
-    },
-];
-
-
 var myTemplate = document.querySelector('.myTemplate');
 var color = document.querySelector('.color');
 var size = document.querySelector('.size')
 var searchShoes = document.querySelector('.shoeStock')
 var li = document.querySelector('.li')
-var shoe = document.querySelector('.shoe')
-
+var displayed = document.querySelector('.display')
 
 var TemplateInstance = Handlebars.compile(myTemplate.innerHTML);
 
-searchShoes.addEventListener('click', function() {
-            var myshoe = [];
-            var mycolor = color.value;
-            var mysize = size.value;
+var shoes =[
+        {color: 'Red', price: 1200,  in_stock: 12,  size: 4,  brand: 'Nike' , image: 'arfox.jpg'},
+        {color: 'Brown', price: 750,  in_stock: 22, size: 5, brand: 'Adidas'},
+        {color: 'Green', price: 550, in_stock: 40,  size: 6,  brand: 'Supegar'},
+        {color: 'Blue', price: 800,  in_stock: 17,  size: 7,  brand: 'Jordan'},
+];
 
-            for (var i = 0; i < shoes.length; i++) {
-                var shoecolor = (shoes[i].color == mycolor || mycolor == 'All')
-                var shoesize = (shoes[i].size == mysize|| mysize == 'All')
-                if (shoecolor) {
-                    shoes.push(shoes)
-
-                }
-                shoe.innerHTML = TemplateInstance({shoes: myshoe})
-
-                }
-              });
+var display = displayed((shoes));
+//displaying
