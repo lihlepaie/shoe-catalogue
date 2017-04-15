b72ff0ec7e80008c751a0d4ba985319a86619430
lihlepaie <pasielihle@gmail.com>
2017-04-15 16:19:52 +0200

    making changes

--- shoe-catalogue.js
+++ shoe-catalogue.js
@@ -3,16 +3,27 @@ var color = document.querySelector('.color');
 var size = document.querySelector('.size')
 var searchShoes = document.querySelector('.shoeStock')
 var li = document.querySelector('.li')
-var displayed = document.querySelector('.display')
+var shoe = document.querySelector('.shoeData')
+var myshoeSize = document.querySelector('.myshoeSize')
+
 
 var TemplateInstance = Handlebars.compile(myTemplate.innerHTML);
 
 var shoes =[
         {color: 'Red', price: 1200,  in_stock: 12,  size: 4,  brand: 'Nike' , image: 'arfox.jpg'},
-        {color: 'Brown', price: 750,  in_stock: 22, size: 5, brand: 'Adidas'},
-        {color: 'Green', price: 550, in_stock: 40,  size: 6,  brand: 'Supegar'},
-        {color: 'Blue', price: 800,  in_stock: 17,  size: 7,  brand: 'Jordan'},
-];
+        {color: 'Brown', price: 750,  in_stock: 22, size: 5, brand: 'Adidas' , image: 'Adidas.jpg'},
+        {color: 'Green', price: 550, in_stock: 40,  size: 6,  brand: 'Superga' , image: 'superga.jpg'},
+        {color: 'Blue', price: 800,  in_stock: 17,  size: 7,  brand: 'Puma' , image: 'puma.jpg'},
 
-var display = displayed((shoes));
-//displaying
+];
+var myshoe = [];
+
+searchShoes.addEventListener('click', function(){
+  var shoe = myshoeSize.value;
+  myshoe.push(shoes);
+
+  
+
+var shoeHTML = TemplateInstance ({shoe:myshoe})
+myshoeSize.innerHTML = shoeHTML
+  });
