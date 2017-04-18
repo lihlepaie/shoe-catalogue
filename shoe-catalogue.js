3de8045b1a8b76d334f90afe8f563c05d59d6f3e
lihlepaie <pasielihle@gmail.com>
2017-04-18 17:13:58 +0200

    making other changes

--- shoe-catalogue.js
+++ shoe-catalogue.js
@@ -1,5 +1,41 @@
+var shoes = [{
+        color: 'Blue',
+        price: 1200,
+        in_stock: 12,
+        size: 4,
+        brand: 'Nike',
+        image: 'AirMax.jpg'
+    },
+    {
+        color: 'Grey',
+        price: 750,
+        in_stock: 22,
+        size: 5,
+        brand: 'Adidas',
+        image: 'Adidas.jpg'
+    },
+    {
+        color: 'White',
+        price: 550,
+        in_stock: 40,
+        size: 6,
+        brand: 'Superga',
+        image: 'superga.jpg'
+    },
+    {
+        color: 'Black',
+        price: 800,
+        in_stock: 17,
+        size: 7,
+        brand: 'Puma',
+        image: 'puma.jpg'
+    },
+
+];
 var myTemplate = document.querySelector('.myTemplate');
-var color = document.querySelector('.color');
+var colors = document.querySelector('.color');
+var brandSelected = document.querySelector('.brand')
+var sizeSelected = document.querySelector('.size');
 var size = document.querySelector('.size')
 var searchShoes = document.querySelector('.shoeStock')
 var li = document.querySelector('.li')
@@ -9,21 +45,36 @@ var myshoeSize = document.querySelector('.myshoeSize')
 
 var TemplateInstance = Handlebars.compile(myTemplate.innerHTML);
 
-var shoes =[
-        {color: 'Red', price: 1200,  in_stock: 12,  size: 4,  brand: 'Nike' , image: 'arfox.jpg'},
-        {color: 'Brown', price: 750,  in_stock: 22, size: 5, brand: 'Adidas' , image: 'Adidas.jpg'},
-        {color: 'Green', price: 550, in_stock: 40,  size: 6,  brand: 'Superga' , image: 'superga.jpg'},
-        {color: 'Blue', price: 800,  in_stock: 17,  size: 7,  brand: 'Puma' , image: 'puma.jpg'},
 
-];
 var myshoe = [];
 
-searchShoes.addEventListener('click', function(){
-  var shoe = myshoeSize.value;
-  myshoe.push(shoes);
+// size.addEventListener('change', function(evt){
+//   var sizeSelected = evt.terget;
+//   //console.log(sizeSelected.value)
+//   for (var i = 0; i < myshoe.length; i++) {
+//     var color = myshoe[i]
+//     if (color(sizeSelected.value)) {
+//       shoesToDisplay.push(myshoe[i]);
+//     }else {
+//       (brand(sizeSelected.value))
+//       shoesToDisplay.push(myshoe[i]);
+//     }
+//   }
+//   var shoeHTML = TemplateInstance({
+//       shoes: myshoe
+//   })
 
-  
+searchShoes.addEventListener('click', function() {
+    for (var i = 0; i < shoes.length; i++) {
+  if (sizeSelected.value == shoes[i].size || colors.value == shoes[i].color || brandSelected.value == shoes[i].brand ){
+            myshoe.push(shoes[i]);
 
-var shoeHTML = TemplateInstance ({shoe:myshoe})
-myshoeSize.innerHTML = shoeHTML
-  });
+        }
+    }
+
+localStorage
+    var shoeHTML = TemplateInstance({
+        shoes: myshoe
+    })
+    myshoeSize.innerHTML = shoeHTML
+});
